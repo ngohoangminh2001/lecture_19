cbfa2f01ce8043bd8f0f584e7b29b2a8961cc780
ngohoangminh2001 <110652464+ngohoangminh2001@users.noreply.github.com>
2024-12-11 10:39:41 +0100

    Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from simple_calc import sum, sub, mul, div
+
+if __name__ == '__main__':
+    op = int(input('Which operation do you need? (0 = sum, 1 = sub, 2 = mul, 3 = division) '))
+    a = int(input('a = '))
+    b = int(input('b = '))
+    if (op == 0):
+        print('a + b = ', sum(a, b))
+    elif (op == 1):
+        print('a - b = ', sub(a, b))
+    elif (op == 2):
+        print('a * b = ', mul(a, b))
+    elif (op == 3):
+        print('a / b = ', div(a, b), '.')
